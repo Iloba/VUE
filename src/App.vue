710b13576233c735535abda9b06ac825f8929095
Iloba <scarlettsney@gmail.com>
2021-10-01 02:58:10 +0100

    c

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your v Vue.js App"/>
 </template>
 
 <script>
